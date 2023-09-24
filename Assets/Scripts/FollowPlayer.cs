54af7dc2e4ed4fb4f66450cd3ebd76f50df5b8f5
eebydeeby <86206723+eebydeeby@users.noreply.github.com>
2023-09-24 20:39:12 +0100

    Updated camera, fixed plane movement speed

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -7,7 +7,7 @@ public class FollowPlayer : MonoBehaviour
 	// Sets up the variable for the vehicle to be followed
 	public GameObject player; 
 	// Sets up camera position based on an offset from the position of the player object
-	private Vector3 offset = new Vector3(0, 7, -12); 
+	private Vector3 offset;
 	
     void LateUpdate()
     {
