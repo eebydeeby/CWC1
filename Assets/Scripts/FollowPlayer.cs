2b28e80cc4d66e9968e8e5edaadfe386911443d5
eebydeeby <86206723+eebydeeby@users.noreply.github.com>
2023-09-25 01:47:06 +0100

    Updated camera controls

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -1,17 +1,35 @@
+// Script for handling player camera input and moving camera based on distance (offset) from player vehicle's position
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
 {
-	// Sets up the variable for the vehicle to be followed
-	public GameObject player; 
-	// Sets up camera position based on an offset from the position of the player object
-	private Vector3 offset = new Vector3(0, 7, -12); 
-	
+    public GameObject player; // Public variable for handling which object to follow
+    private Vector3 offset; // Handles how far to distance camera away from player object
+    private float cameraInput; // Handles player input for camera controls
+
     void LateUpdate()
     {
-		// Updates the camera position
-		transform.position = player.transform.position + offset;
+        // Variable for handling camera control input
+		// Jump (space) key is used to move camera
+        cameraInput = Input.GetAxis("Jump");
+		
+        // Checks if player is holding down the jump key
+        if (cameraInput == 0)
+        {
+            // Moves camera offset to behind and above the vehicle
+            offset = new Vector3(0, 5, -7);
+        }
+        else
+        {
+            // Moves camera offset to the front of the vehicle
+            offset = new Vector3(0f, 3f, 10f);
+        }
+		
+        // Updates the camera position and rotation with the current camera offset and vehicle rotation
+        transform.position = player.transform.position + offset;
+		transform.rotation = player.transform.rotation;
     }
 }
