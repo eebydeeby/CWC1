a4beeab4a23389b0b27f93e4e25eef03ad9dd237
eebydeeby <86206723+eebydeeby@users.noreply.github.com>
2023-11-21 04:06:33 +0000

    Replaced transform.translate with forces

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-	[SerializeField] float speed = 20.0f;
+	[SerializeField] float horsePower = 20.0f;
 	[SerializeField] float turnSpeed = 45.0f;
 	private float horizontalInput;
 	private float forwardInput;
+	private Rigidbody playerRb;
+
+	void Start(){
+		playerRb = GetComponent<Rigidbody>();
+	}
 
     // Update is called once per frame
     void FixedUpdate()
@@ -17,7 +22,7 @@ public class PlayerController : MonoBehaviour
 		forwardInput = Input.GetAxis("Vertical");
 		
 		// Moves the vehicle forward based on vertical input
-		transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+		playerRb.AddRelativeForce(Vector3.forward * forwardInput * horsePower);
 		
 		//Rotates the car based on horzontal input
 		transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
