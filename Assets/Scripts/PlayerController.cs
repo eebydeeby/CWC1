cbf15f5cf148d50d1fd47a33fd4e54ec55dee9bc
eebydeeby <86206723+eebydeeby@users.noreply.github.com>
2023-11-21 02:51:38 +0000

    Refactored, fixed frame issues

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-	private float speed = 20.0f;
-	private float turnSpeed = 45.0f;
+	[SerializeField] float speed = 20.0f;
+	[SerializeField] float turnSpeed = 45.0f;
 	private float horizontalInput;
 	private float forwardInput;
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
 		// Sets up input parameters using Unity input API
 		horizontalInput = Input.GetAxis("Horizontal");
--- README.md
+++ README.md
@@ -1,8 +1,3 @@
 # CWC1
 
-Base game for the Create With Code challenges.
-
-This game is not submitted as an assignment.
-
-For the Challenge game (plane sidescroller), go [here](https://github.com/eebydeeby/CWC1/tree/Challenge-1).  
-For the Bonus game (multiplayer buses), go [here](https://github.com/eebydeeby/CWC1/tree/bonus-1).
+Updated version of Prototype 1 for Create With Code Lab 6.1.
