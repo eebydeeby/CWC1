955ba35fc685cba98ba87f2bae0304c6a93d8eb3
eebydeeby <86206723+eebydeeby@users.noreply.github.com>
2023-11-21 05:51:35 +0000

    Cleaned up code

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,15 +5,18 @@ using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
-	[SerializeField] float horsePower = 20.0f;
+	[SerializeField] float horsePower = 20.0f; // Amount of force applied to car when moving
 	[SerializeField] float turnSpeed = 45.0f;
-	[SerializeField] GameObject centerOfMass;
+	[SerializeField] GameObject centerOfMass; // Determines where centre of mass for car is
+	
 	[SerializeField] TextMeshProUGUI speedometerText;
-	[SerializeField] float speed;
+	[SerializeField] float speed; // Speed to be displayed
 	[SerializeField] TextMeshProUGUI rpmText;
-	[SerializeField] float rpm;
-    [SerializeField] List<WheelCollider> allWheels;
+	[SerializeField] float rpm; // RPM to be displayed
+	
+    [SerializeField] List<WheelCollider> allWheels; // Gets a list of all wheel colliders
     [SerializeField] int wheelsOnGround;
+	
 	private float horizontalInput;
 	private float forwardInput;
 	private Rigidbody playerRb;
@@ -23,6 +26,7 @@ public class PlayerController : MonoBehaviour
 		playerRb.centerOfMass = centerOfMass.transform.localPosition;
 	}
 
+	// Updates text on screen with speed and RPM
 	void Update(){
 		speed = Mathf.Round(playerRb.velocity.magnitude);
 		speedometerText.SetText("Speed: " + speed + " mph");
@@ -36,47 +40,41 @@ public class PlayerController : MonoBehaviour
 		horizontalInput = Input.GetAxis("Horizontal");
 		forwardInput = Input.GetAxis("Vertical");
 		
-		// Moves the vehicle forward based on vertical input
-		//playerRb.AddRelativeForce(Vector3.forward * forwardInput * horsePower);
-		
-		//Rotates the car based on horzontal input
-		//transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
-		
-	    if (IsOnGround())
-	           {
+	    if (IsOnGround()) // Only applies forces if all 4 wheels ar on ground
+	    	{
+	  		   // Move the vehicle forward
+	           playerRb.AddRelativeForce(Vector3.forward * forwardInput * horsePower);
+	           
+			   // Turning the vehicle
+	           transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
 
-	               //Move the vehicle forward
-	               //transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
-	               playerRb.AddRelativeForce(Vector3.forward * forwardInput * horsePower);
-	               //Turning the vehicle
-	               transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
+	           // Print speed
+	           speed = Mathf.RoundToInt(playerRb.velocity.magnitude * 2.237f);
+	           speedometerText.SetText("Speed: " + speed + " mph");
 
-	               //print speed
-	               speed = Mathf.RoundToInt(playerRb.velocity.magnitude * 2.237f);
-	               speedometerText.SetText("Speed: " + speed + " mph");
+	           // Print RPM
+	           rpm = Mathf.Round((speed % 30) * 40);
+	           rpmText.SetText("RPM: " + rpm);
+	       	}
+		}
 
-	               //print RPM
-	               rpm = Mathf.Round((speed % 30) * 40);
-	               rpmText.SetText("RPM: " + rpm);
-	           }
-	       }
-
-	       bool IsOnGround()
-	       {
-	           wheelsOnGround = 0;
-	           foreach (WheelCollider wheel in allWheels)
-	           {
-	               if (wheel.isGrounded)
-	               {
-	                   wheelsOnGround++;
-	               }
-	           }
-	           if (wheelsOnGround == 4)
-	           {
-	               return true;
-	           } else
-	           {
-	               return false;
-	       }
-	   }
+	// Checks if all 4 wheel colliders are colliding with ground and returns a bool
+    bool IsOnGround()
+       {
+           wheelsOnGround = 0;
+           foreach (WheelCollider wheel in allWheels)
+           {
+               if (wheel.isGrounded)
+               {
+                   wheelsOnGround++;
+               }
+           }
+           if (wheelsOnGround == 4)
+           {
+               return true;
+           }
+	       else {
+ 	  	       return false;
+           }
+       }
 }
